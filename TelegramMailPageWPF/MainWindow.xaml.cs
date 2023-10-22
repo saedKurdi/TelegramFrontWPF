5a51e340b3171ed01a21e5de00b0b2ecf7e09c09
= <=>
2023-10-22 12:12:06 +0400

    Telegram Front End WPF

---
+++ TelegramMailPageWPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelegramMailPageWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mouse_enter_to_search(object sender, MouseEventArgs e)
+        {
+            search_textbox.Text = "";
+        }
+
+        private void mouse_leave_to_search(object sender, MouseEventArgs e)
+        {
+            search_textbox.Text = "Search";
+        }
+
+        private void mouse_enter_to_write_message(object sender, MouseEventArgs e)
+        {
+            write_message_txtbox.Text = string.Empty;
+        }
+
+        private void mouse_leave_to_write_message(object sender, MouseEventArgs e)
+        {
+            write_message_txtbox.Text = "Write a message . . .";
+        }
+    }
+}
